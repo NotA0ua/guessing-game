9db7141e4685bbba64d0a2b113bf1a8e49d0a35b
NotA0ua <demid.dubrovsky@yandex.ru>
2024-05-28 11:40:40 +0300

    complete the game

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,30 @@ fn main() {
 
     let guessed_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Загаданное число: {guessed_number}");
-    println!("Введите число: ");
+    loop {
+        println!("Введите число: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Ошибка при чтении строки");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Ошибка при чтении строки");
 
-    let guess: i32 = guess.trim().parse().expect("Вы ввели не число!");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Вам нужно ввести число!");
+                continue;
+            },
+        };
 
-    match guess.cmp(&guessed_number){
-        Ordering::Less => println!("Слишком маленькое число!"),
-        Ordering::Equal => println!("Вы угадали!!!"),
-        Ordering::Greater => println!("Слишком большое число!"),
+        match guess.cmp(&guessed_number) {
+            Ordering::Less => println!("Слишком маленькое число!"),
+            Ordering::Greater => println!("Слишком большое число!"),
+            Ordering::Equal => {
+                println!("Вы угадали!!!");
+                break;
+            }
+        }
     }
 }
